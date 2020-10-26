fadf8beca24c504a101c0368db732a4a8d9e07dc
absurdtales <hunter.barrett9@gmail.com>
2020-10-26 11:44:41 +1000

    Week 4 Lesson 1

---
+++ pitchrollyaw.py
@@ -0,0 +1,15 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+while True:
+    o = sense.get_orientation()
+    
+print(o)
+
+pitch = o["pitch"]
+roll = o["roll"]
+yaw = o["yaw"]
+
+print(f"Pitch:{pitch}\tRoll:{roll}\tYaw:{yaw}")
