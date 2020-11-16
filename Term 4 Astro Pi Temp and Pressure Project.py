46b516ca085f73c0e93c04a92607586baee0dbd4
absurdtales <hunter.barrett9@gmail.com>
2020-11-16 11:48:31 +1000

    bruh

--- Term 4 Astro Pi Temp and Pressure Project.py
+++ Term 4 Astro Pi Temp and Pressure Project.py
@@ -24,12 +24,25 @@ message = "Temp: " + str(temp) + " Pressure: " + str(pressure)
 # Average temperature on the ISS is around 24 degrees celcius, so I've given the normal levels a 10 degree leeway.
 # Pressure on ISS is 101.3 kPa, anything other than this value could ruin equipment.
 # Anything outside of these values would indicate the temperature sensor is malfunctioning on the ISS, and the pressure is wack.
-if temp > 20 and temp < 30 and pressure == 1013:
+if temp > 20 and temp < 50 and pressure == 1013:
     bg = green
-    sense.show_message("Temp Levels Normal: " + str(temp), scroll_speed)
+    sense.show_message(f"Temp and Pressure Levels Normal: {temp}, {pressure}")
 else:
     bg = red
-    ("WARNING TEMP LEVELS ABNORMAL: " + str(temp))
+    (f"WARNING TEMP AND PRESSURE LEVELS ABNORMAL: {temp}, {pressure}")
 
-# This line of code tells the SenseHat to showcase the message and background colour 
-sense.show_message(message, scroll_speed = 0.075, back_colour = bg)
+
+
+# This code will allow the screen to be turned off when pressing the joystick
+while True:
+    for event in sense.stick.get_events():
+        if event.action == 'pressed':
+            
+            if event.direction == 'up':
+                sense.clear()
+            elif event.direction == 'down':
+                sense.clear()
+            elif event.direction == 'right':
+                sense.clear()
+            elif event.direction == 'left':
+                sense.clear()
